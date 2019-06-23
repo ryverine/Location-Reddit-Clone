2699ca00442e7358ba456799c3708f212bb6071a
ryverine <rwcheek@gmail.com>
2019-06-23 16:18:21 -0400

    Updating database SCHEMA and SEEDS files.

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+-- DROP DATABASE IF EXISTS exampledb;
+CREATE DATABASE store_finder_db;
+USE store_finder_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+
---
+++ models/seeds.sql
@@ -0,0 +1,59 @@
+
+	INSERT INTO locations (city, state)
+    VALUES
+		("Durham","NC"),
+		("Chapel Hill","NC"),
+        ("Raleigh","NC"),
+        ("Greensboro","NC"),
+        ("Charlotte","NC");
+
+
+	INSERT INTO stores (name, description, addrln1, addrln2, zip, LocationID)
+    VALUES
+		("Cloud Vape Lounge","","88 Gobo St","","11111","1"),
+		("Tobacco land & Vape Shop CBD Kratom Shop","","221 Frog Hop Ln","","11112","1"),
+		("Smoke Rings Smoke Shop","","733 Crab Claw Ct","","11113","1"),
+		("Bubba's Bud Shop","","123 Main St","","21111","2"),
+        ("Vape Naysh","","227 Green Ave","Block #34","21112","2"),
+        ("Vapor River","","48 Bop-town Rd","","21113","2"),
+		("TOBACCO OUTLET & Vape Shop","","734 Blam St","","31111","3"),
+        ("The Wick And Coil Vape Shop","","86 Rocky Ln","","31112","3"),
+        ("Countryside Vaporz","","1229 Bobo St","","31113","3"),
+        ("Vapers Ink","","144 Blue St","","41111","4"),
+        ("Stratus Smoke & Vape","","12 Yellow Rd","","41112","4"),
+        ("AVAIL Vapor","","889 Red Cr","","41113","4"),
+        ("A and D Vapors","","729 Jackson Sq","","51111","5"),
+        ("Local Liquids Vape Shop","","832 Washington St","","51112","5"),
+        ("Haven Smoke & Vape","","2030 Williams Ln","","51113","5");
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
