ff70a515c3c3d8e21532e7d13cfc514d28785a78
ryverine <rwcheek@gmail.com>
2019-06-24 12:46:14 -0400

    Created PRODUCTS table. Each product belongs to a single store, and each store can have many products. This means that a product that is in multipe stores will need to have multiple records in the PRODUCTS table, each matching to a single STORE. When viewing a STORE page you will get a list of the PRODUCTS for that store.

---
+++ models/product.js
@@ -0,0 +1,66 @@
+
+
+module.exports = function(sequelize, DataTypes) 
+{
+    var Product = sequelize.define("Product", {
+      name:
+      {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [3]
+          }
+      },
+      description:
+      {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+            len: [3]
+          }
+      },
+      brand: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [2]
+          }
+      },
+      quantity:
+      { 
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+            min: 0
+          }
+        },
+      price:
+      { 
+          type: DataTypes.FLOAT,
+          allowNull: true,
+          validate: {
+            min: 0
+          }
+        },
+     },
+    {
+      timestamps: false
+    });
+    
+    Product.associate = function(models) 
+    {
+        Product.belongsTo(models.Store, {
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      };
+
+    return Product;
+  };
+  
+
+
+
+
+
